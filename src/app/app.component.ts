4a8fc770554d08759b75b7d70ae4320d4981bd78
joemafu <joe_mafu@hotmail.com>
2024-04-20 02:11:45 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from 'src/environments/environment';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Initialize Firebase
+const app = initializeApp(environment.firebaseConfig);
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, AngularFireAuthModule, AngularFireModule],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    AngularFireModule.initializeApp(environment.firebaseConfig);
+  }
 }
